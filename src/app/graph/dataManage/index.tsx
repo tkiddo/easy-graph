d73bbd7ad0302563564cee3035985f4581a04c0c
tkiddo <justforgit@163.com>
2023-05-06 13:44:33 +0800

    feat: data manage tab

--- src/app/graph/dataManage/index.tsx
+++ src/app/graph/dataManage/index.tsx
@@ -1,28 +1,56 @@
-import styles from './index.module.css'
-import { Button, Upload } from 'antd'
+// import styles from './index.module.css'
+import { Button, Upload, Table, Divider } from 'antd'
 import type { UploadChangeParam } from 'antd/lib/upload'
+import { useState } from 'react'
 
 export default function DataManage() {
+  const [list, setList] = useState([])
+
+  const columns = list[0]
+    ? Object.keys(list[0]).map((key) => {
+        return {
+          key,
+          dataIndex: key,
+          title: key,
+        }
+      })
+    : []
+
   const handleChange = (param: UploadChangeParam) => {
-    const { file, fileList } = param
-    console.log(file, fileList)
+    const { file } = param
+    const { type } = file
     const fileReader = new FileReader()
     fileReader.onload = (e) => {
       const result = e.target?.result
-      console.log(result)
+      if (type === 'application/json') {
+        setList(JSON.parse(result as string))
+      }
     }
-    // fileReader.readAsBinaryString(file)
+    fileReader.readAsBinaryString(file as any)
+  }
+
+  const beforeUpload = () => {
+    return false
   }
 
   return (
-    <div className={styles.container}>
+    <main>
       <Upload
         accept=".json,.csv"
         onChange={handleChange}
-        beforeUpload={() => false}
+        beforeUpload={beforeUpload}
+        showUploadList={false}
       >
         <Button type="primary">打开文件</Button>
       </Upload>
-    </div>
+      <Divider />
+      <Table
+        dataSource={list}
+        pagination={{
+          pageSize: 10,
+        }}
+        columns={columns}
+      />
+    </main>
   )
 }
